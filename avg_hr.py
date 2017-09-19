25ad6691423a7e737e45f506539dcc7465cd3ec9
iel2 <iel2@duke.edu>
2017-09-19 15:43:39 -0400

    Last commit added unwanted txt in functions. Reformatted.

--- avg_hr.py
+++ avg_hr.py
@@ -1,18 +1,11 @@
 # Need to import Niranjana's Module for Inst. HR
-<<<<<<< HEAD
+
 from inst_hr import inst_hr
 
 # Initialization
 min2sec = 60
 inst_time_period = 5  # In minutes
-=======
 
-# Headers
-min2sec = 60
-# Ask Niranjana what she called this variable
-blockwindow = 5
-
->>>>>>> 7d7b8cccf8e981168cef7d796688fa1231140d93
 
 def new_hr_set(mins):
     # Taking input and converting it to seconds
@@ -25,7 +18,6 @@ def new_hr_set(mins):
     usersec = mins * min2sec
 
     # Must have number of groups be whole
-<<<<<<< HEAD
     if usersec % inst_time_period is True:
         groupnum = usersec/inst_time_period
     # Taking an additional groups if user input is between groups
@@ -37,24 +29,8 @@ def new_hr_set(mins):
     realbunches = rawbunches[:groupnum]
     return realbunches, groupnum
 
-
-def avg_hr(realbunches, groupnum):
-    avghr = sum(realbunches)/groupnum
-=======
-    if usersec % blockwindow is True:
-        groupnum = usersec/blockwindow
-    # Taking an additional groups if user input is between groups
-    elif usersec % blockwindow is False:
-        groupnum = round(usersec/blockwindow) + 1
-
-    # Take Niranjana's grouping output and truncating to fit user input
-    bunches = niranj()
-    return bunches, groupnum
-
-
 def avg_hr(bunches,groupnum):
     avghr = sum(bunches)/groupnum
->>>>>>> 7d7b8cccf8e981168cef7d796688fa1231140d93
     return avghr()
     print('The average heart rate for {} minute(s) was {} beats per minute'.format(mins, avghr))
 
