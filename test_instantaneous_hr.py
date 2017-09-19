660774147991f70c04eaa29837abc0c667d7ac38
NinjMenon <ns229@duke.edu>
2017-09-19 13:40:22 -0400

    Cleaned unit test for inst_hr function. Need to add more assert conditions

--- test_instantaneous_hr.py
+++ test_instantaneous_hr.py
@@ -2,33 +2,29 @@ import find_max_peaks
 import inst_hr
 import numpy as np
 
-sample_voltage_array = np.array([0.411, 0.388, 0.3345, 0.386, 0.3475, 0.35, 0.3575, 0.3645, 0.3515, 0.324, 0.2965])
-sample_time_array = np.array([8,8.002,8.004,8.006,8.008,8.01,8.012,8.014,8.016,8.018,8.02])
+sample_voltage_array = np.array([0.411, 0.388, 0.3345, 0.386, 0.3475]) #in mv
+sample_time_array = np.array([0,1,2,3,4])#in seconds
 
-# TODO: Use bigger data sets
-#TODO: Update : This is a teriible unit test. To be rescripted today 9/19
-
+#TODO: Use bigger data sets to get multiple cardiac beats
+#TODO: Use data sets with of length that is not fully divisible by 5. Want : func ignores leftover values
 
 def test_find_max_peaks():
     """
-    Unit test for function that finds R peaks of all QRS modules in the ECG trace
+    Unit test for function that finds and counts R peaks of all QRS modules within specified inst_time_period_secs
     """
-    index_of_peaks = find_max_peaks(sample_voltage_array)
+    index_of_peaks = find_max_peaks(sample_voltage_array,inst_time_period_secs = 5)
 
-    assert index_of_peaks == [3, 7]
-    assert len(index_of_peaks) == 2
+    assert index_of_peaks == [1,3]
     return len(index_of_peaks)
 
 
 def test_inst_hr():
     """
     Unit test for function that calculates instantaneous HR
-    :return: pass if assertion is true
+    :return: nothing, pass if assertion is true
     """
-    index_of_peaks = test_find_max_peaks()
-    indexed_time = sample_time_array[index_of_peaks]
-
-    assert inst_hr(indexed_time) == 30000 # unrealistic but just checking the math
+    num_of_peaks = test_find_max_peaks()
+    assert inst_hr(num_of_peaks) == 24 #in bpm  
 
 
 
