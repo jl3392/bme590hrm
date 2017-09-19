cd7b2e616b11265da0440f0883891a0b1b1cbb84
NinjMenon <ns229@duke.edu>
2017-09-19 11:39:51 -0400

    Changed test_find_max_peaks(). Need to rescript this complete test

--- test_instantaneous_hr.py
+++ test_instantaneous_hr.py
@@ -1,11 +1,12 @@
-from hrm1 import find_max_peaks
-from hrm1 import inst_hr
+import find_max_peaks
+import inst_hr
 import numpy as np
 
 sample_voltage_array = np.array([0.411, 0.388, 0.3345, 0.386, 0.3475, 0.35, 0.3575, 0.3645, 0.3515, 0.324, 0.2965])
 sample_time_array = np.array([8,8.002,8.004,8.006,8.008,8.01,8.012,8.014,8.016,8.018,8.02])
 
 # TODO: Use bigger data sets
+#TODO: Update : This is a teriible unit test. To be rescripted today 9/19
 
 
 def test_find_max_peaks():
@@ -14,8 +15,9 @@ def test_find_max_peaks():
     """
     index_of_peaks = find_max_peaks(sample_voltage_array)
 
-    assert index_of_peaks == [3, 7]  
-    return index_of_peaks
+    assert index_of_peaks == [3, 7]
+    assert len(index_of_peaks) == 2
+    return len(index_of_peaks)
 
 
 def test_inst_hr():
