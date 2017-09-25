680ea2c36911d2a92b9f2757423cc6713af338d3
iel2 <iel2@duke.edu>
2017-09-25 19:11:58 -0400

    Added Sphinx-friendly docstrings. Changed how thresholds were referenced.

--- test_bradtach.py
+++ test_bradtach.py
@@ -5,15 +5,24 @@ from bradtach import bradtach
 
 
 def test_normal():
-    assert bradtach(80, 100, 60) == 2
+    avg_hr = 80  # Normal Adult resting HR
+    mins = 1
+    testnormal = bradtach(avg_hr, mins)
+    assert testnormal[0] == 2
 
 
 def test_brady():
-    assert bradtach(30, 100, 60) == 0
+    avg_hr = 60  # Adult bradycardia
+    mins = 1
+    testbrady = bradtach(avg_hr, mins)
+    assert testbrady[0] == 0
 
 
 def test_tachy():
-    assert bradtach(120, 100, 60) == 1
+    avg_hr = 120  # Adult tachycardia
+    mins = 1
+    testtach = bradtach(avg_hr, mins)
+    assert testtach[0] == 1
 
 
 
