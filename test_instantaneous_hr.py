3027450887c9ccb405cecff791f199abcc6aefb2
NinjMenon <ns229@duke.edu>
2017-09-24 20:48:03 -0400

    Fixed naming of variables to meet other files

--- test_instantaneous_hr.py
+++ test_instantaneous_hr.py
@@ -1,5 +1,5 @@
 import find_max_peaks
-import numpy as np
+import numpy
 
 sample_voltage_array1 = np.array([0.411, 0.388, 0.3345, 0.386, 0.3475]) #in mv
 sample_time_array1 = np.array([0,1,2,3,4])#in seconds
@@ -15,9 +15,9 @@ def test_find_max_peaks():
     """
     Unit test for function that finds and counts R peaks of all QRS modules within specified inst_time_period_secs
     """
-    index_of_peaks1 = find_max_peaks(sample_voltage_array1,inst_time_period = 5)
-    index_of_peaks2 = find_max_peaks(sample_voltage_array2, inst_time_period = 5)
-    index_of_peaks3 = find_max_peaks(sample_voltage_array3, inst_time_period = 5)
+    index_of_peaks1 = find_max_peaks(sample_voltage_array1,update_time = 5)
+    index_of_peaks2 = find_max_peaks(sample_voltage_array2, update_time = 5)
+    index_of_peaks3 = find_max_peaks(sample_voltage_array3, update_time = 5)
 
     assert index_of_peaks1 == [1,3]
     assert index_of_peaks2 == [[1,3],[6,8]]
