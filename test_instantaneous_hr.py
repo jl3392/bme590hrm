add414bb43567984aec24213c7739d6cdfa6abdb
NinjMenon <ns229@duke.edu>
2017-09-25 18:28:18 -0400

    Fixed some PEP8 errors

--- test_instantaneous_hr.py
+++ test_instantaneous_hr.py
@@ -12,9 +12,12 @@ def test_find_max_peaks():
     Unit test for function that finds and counts R peaks of all QRS modules within specified inst_time_period_secs
     """
     peaks = find_max_peaks(voltage_array=sample_voltage_array, time_array=sample_time_array, min_dist=150,
-                             update_time=3)
-    assert peaks == [[[8.6739999999999995, 0.54249999999999998], [9.3260000000000005, 0.59950000000000003]], [[9.9879999999999995, 0.63800000000000001], [10.654, 0.64949999999999997]], [[11.318, 0.66849999999999998], [11.952, 0.64749999999999996]]]
+                           update_time=3)
+    assert peaks == [[[8.6739999999999995, 0.54249999999999998], [9.3260000000000005, 0.59950000000000003]],
+                     [[9.9879999999999995, 0.63800000000000001], [10.654, 0.64949999999999997]],
+                     [[11.318, 0.66849999999999998], [11.952, 0.64749999999999996]]]
     return peaks
+
 
 def test_inst_hr():
     """
@@ -25,10 +28,7 @@ def test_inst_hr():
     from inst_hr import inst_hr
     peaks = test_find_max_peaks()
 
-    HR = inst_hr(peaks,update_time =3)  #in bpm
-    checking_HR = np.array(HR == [40,40,40])
+    hr = inst_hr(peaks, update_time=3)  # in bpm
+    checking_hr = np.array(hr == [40, 40, 40])
 
-    assert checking_HR.all()
-
-
-
+    assert checking_hr.all()
