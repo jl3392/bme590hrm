dbc2d5b9a78bfac555f6d46114487e53b19e927e
iel2 <iel2@duke.edu>
2017-09-25 18:55:39 -0400

    Added Sphinx-friendly docstrings to test and working modules.

--- avg_hr.py
+++ avg_hr.py
@@ -3,12 +3,39 @@
 import math
 
 
+""" Average Heart Rate.
+
+This module takes instantaneous HR inputs to output an average HR 
+over a time period specified by the user in minutes.  
+
+"""
+
+
 # Initialization
 min2sec = 60
 inst_time_period = 5  # In seconds
 
 
 def new_hr_set(mins, rawbunches):
+    """ returns realbunches, groupnum
+
+    This function takes in raw instantaneous heart rate based on an averaging window set by the user, as well as minutes
+    specified by user input and returns groupnum and realbunches. Groupnum is the result of taking the user input,
+    converting it to seconds, and then dividing that number by the amount of the instantaneous heart rate window
+    duration (specified in our case as 5 seconds). This is effectively saying the user wants X amount of bunches from
+    the instantaneous HR data. Realbunches is a list that is the instantaneous heart rate array
+    truncated to go to "groupnum" amount of bunched sets.
+
+    :param mins: minutes
+    :param rawbunches: raw instantaneous heart rate data
+    :type mins: str (from user)
+    :type rawbunches: list
+    :return: realbunches - truncated instantaneous heart rate data set
+    :return: groupnum - number of groups to take out of the raw inst HR set, based on user input
+    :rtype realbunches: list
+    :rtype groupnum: int
+
+    """
     # Taking input and converting it to seconds
     if isinstance(mins, float) is True or isinstance(mins, int) is True:
         mins = mins
@@ -37,9 +64,21 @@ def new_hr_set(mins, rawbunches):
 
 
 def avg_hr(realbunches, groupnum):
+    """ returns avghr
+
+    This function takes in the outputs of new_hr_set to get the averaged HR over the user specified amount of time.
+
+    :param realbunches: Truncated inst HR data set
+    :param groupnum: # of groups
+    :type realbunches: list
+    :type groupnum: int
+    :return avghr: The average HR over the amount of time (min) specified by the user
+    :rtype avghr: float, int
+
+    """
     avghr = math.floor(sum(realbunches)/groupnum)
     return avghr
-    print('The average heart rate for {} minute(s) was {} beats per minute'.format(mins, avghr))
+    # print('The average heart rate for {} minute(s) was {} beats per minute'.format(mins, avghr))
 
 
 
