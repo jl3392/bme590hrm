379cd6bac4b4883ca533e305d2098ec6cc9688a3
iel2 <iel2@duke.edu>
2017-09-24 20:52:38 -0400

    Pep8 cleaning

--- bradtach.py
+++ bradtach.py
@@ -2,8 +2,8 @@
 # from avg_hr import avg_hr
 
 # Initializing variables
-bradythreshold = 60 # For normal adults
-tachythreshold = 100 # For normal adults
+bradythreshold = 60  # For normal adults
+tachythreshold = 100  # For normal adults
 
 
 def bradtach(avg_hr, tachythreshold, bradythreshold):
--- test_bradtach.py
+++ test_bradtach.py
@@ -1,13 +1,16 @@
 # Importing Dependencies
 
-# Importing necessary fxns
+# Importing necessary functions
 from bradtach import bradtach
+
 
 def test_normal():
     assert bradtach(80, 100, 60) == 2
 
+
 def test_brady():
     assert bradtach(30, 100, 60) == 0
+
 
 def test_tachy():
     assert bradtach(120, 100, 60) == 1
