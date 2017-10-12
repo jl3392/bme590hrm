7f5ea89138abac23ac258a37bdc5eeb048092bd6
iel2 <iel2@duke.edu>
2017-10-11 21:00:19 -0400

    Changed input - now user inputs one averaging window for all 30 files instead of inputting 30 times.

--- hrm1.py
+++ hrm1.py
@@ -16,7 +16,7 @@ period of time.
 """
 
 
-def hrmonitor(filepath,output_name):
+def hrmonitor(filepath, output_name):
     """
     :param filepath: ECG data location
     :param output_name : Output file name
@@ -30,28 +30,29 @@ def hrmonitor(filepath,output_name):
     maxpeaks = find_max_peaks(voltage, time, update_time=5)
     instantaneoushr = inst_hr(maxpeaks, update_time=5)
     # Average heart rate
-    mins = float(input('Please specify a time (in min) for averaging.'))
-    newHRset = new_hr_set(mins, instantaneoushr, update_time=5)
-    avgHR = avg_hr(newHRset[0])
+    # mins = float(input('Please specify a time (in min) for averaging.'))
+    newhrset = new_hr_set(mins, instantaneoushr, update_time=5)
+    avghr = avg_hr(newhrset[0])
     # Brady/Tachycardia
-    condition = bradtach(avgHR, mins)
+    condition = bradtach(avghr, mins)
     message = condition[1]
     # Output Information in .txt File
     HRinfo = open('{}_HR_Information.txt'.format(output_name), 'w')
     HRinfo.write("Estimated Instantaneous HR is {} beats per minute."
                  .format(instantaneoushr))
     HRinfo.write("/nEstimated Average HR is {} beats per minute in minutes/n"
-                 .format(avgHR, mins))
+                 .format(avghr, mins))
     HRinfo.write("/n{}/n".format(message))
 
 if __name__ == "__main__":
     # Importing csv data
     # Create the list of file
     list_of_files = glob.glob('test_data/*.csv')
+    mins = float(input('Please specify a time (in min) for averaging.'))
     for file_name in list_of_files:
         FI = open(file_name, 'r')
         output_name = file_name[:-4]
-        hrmonitor(filepath = FI,output_name=output_name)
+        hrmonitor(filepath=FI, output_name=output_name)
         FI.close()
 
     # exampledata = opencsv('Data/ecg_data.csv')
