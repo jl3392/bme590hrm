3d96d2a8300c1b11085d97ce09f52d89ac62aa45
iel2 <iel2@duke.edu>
2017-10-15 13:03:40 -0400

    No interaction for minutes,default value instead.

--- get_ecg.py
+++ get_ecg.py
@@ -11,7 +11,7 @@ class GetEcg:
     min2sec = 60
 
     def __init__(self, csv_file=None, update_time=5,
-                 brady_threshold=60, tachy_threshold=100):
+                 brady_threshold=60, tachy_threshold=100, mins=2):
         """
         Constructor for ECG processing needs the input csv file to work on and
         time for often to update the instantaneous HR
@@ -29,6 +29,7 @@ class GetEcg:
             self.update_time = update_time
             self.brady_threshold = brady_threshold
             self.tachy_threshold = tachy_threshold
+            self.mins = mins
 
         self.raw_bunches = []
         self.total_peaks = []
@@ -129,28 +130,23 @@ class GetEcg:
     def get_avghr(self):
         """ returns avghr
 
-            Function gets avghr over user-specified minutes window.
-            Also gives clinical indication of tachy/bradycardia
+            Method gets avghr over user-specified minutes window.
+            Also gives clinical indication of tachy/bradycardia,
+            based on threshold values
 
             :return avg_hr: Avg hr
             :return status: Brady or tachycardia
         """
-        mins = float(input('Please specify a time (in min) for averaging.'))
-        if isinstance(mins, float) is True or isinstance(mins, int) is True:
-            mins = mins
-        elif isinstance(mins, complex) is True:
-            raise ValueError('Please use real numbers.')
 
-        user_sec = mins * self.min2sec
+        user_sec = self.mins * self.min2sec
 
-    # Must have number of groups be whole
+    # Get number of groups based off of time
         if user_sec % self.update_time == 0:
             group_num = int(user_sec/self.update_time)
-        # Taking an additional groups if user input is between groups
+    # Taking an extra group if user input mins is between groups
         else:
             group_num = int(math.floor(user_sec/self.update_time) + 1)
 
-        # Take Niranjana's grouping output and truncating to fit user input
         if len(self.raw_bunches) <= group_num:  # if user time is > raw data
             real_bunches = self.raw_bunches
         else:
@@ -162,12 +158,12 @@ class GetEcg:
     # Calculating brady-/tachycardia
         if self.brady_threshold < avg_hr < self.tachy_threshold:
             status = [2, 'You have a normal average heart rate '
-                         'over the period of {} minutes'.format(mins)]
+                         'over the period of {} minutes'.format(self.mins)]
         elif avg_hr >= self.tachy_threshold:
             status = [1, 'You have tachycardia over the '
-                         'period of {} minutes'.format(mins)]  # Tachy
+                         'period of {} minutes'.format(self.mins)]  # Tachy
         elif avg_hr <= self.brady_threshold:
             status = [0, 'You have bradycardia over '
-                         'the period of {} minutes'.format(mins)]  # Brady
+                         'the period of {} minutes'.format(self.mins)]  # Brady
 
         return [status, avg_hr]
